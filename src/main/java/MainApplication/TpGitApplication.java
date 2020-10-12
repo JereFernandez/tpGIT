3c05c29f0034a01b8967095fc090939341730711
Jeremias Fernandez <jere_69@live.com>
2020-10-12 18:53:22 -0300

    commit initial
    commit initial

---
+++ src/main/java/MainApplication/TpGitApplication.java
@@ -0,0 +1,13 @@
+package MainApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpGitApplication.class, args);
+	}
+
+}
