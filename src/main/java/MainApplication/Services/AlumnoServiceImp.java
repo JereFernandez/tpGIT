295ee79d57e7f74b6a253a3c5e7b8cc4d762a886
Jeremias Fernandez <jere_69@live.com>
2020-10-14 17:53:27 -0300

    Configuración y arreglo. Se configuró la DB, agregué un atributo en alumno y cambié el nombre de la tabla

--- src/main/java/MainApplication/Services/AlumnoServiceImp.java
+++ src/main/java/MainApplication/Services/AlumnoServiceImp.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class AlumnoServiceImp extends BaseServiceImpl <Alumno, Long> implements AlumnoService {
+
     @Autowired
     private AlumnoRepository alumnoRepository;
 
--- src/main/java/MainApplication/entities/Alumno.java
+++ src/main/java/MainApplication/entities/Alumno.java
@@ -4,10 +4,11 @@ import lombok.*;
 import org.hibernate.envers.Audited;
 import javax.persistence.Entity;
 import javax.persistence.*;
+import java.util.Date;
 
 
 @Entity
-@Table(name = "persona")
+@Table(name = "alumno")
 //@Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -25,6 +26,9 @@ public class Alumno extends Base {
     private int email;
     @Column(name="foto")
     private byte foto;
+    @Column(name="creatAl")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createAl;
 
     
 }
